d8139ffa1268c38c759af8971bda465d08cc8df7
nwehla <nwehlabatoum@laposte.net>
2022-03-02 16:32:25 +0100

    Début du projet Quideance

---
+++ documents/descriptif_application/dictionnaire.groovy
@@ -0,0 +1,27 @@
+Mots :
+    -admin, super admin
+    -api (à voir si besoin)
+    -connecter
+    -déconnecter
+    -utilisateur
+    -questionnaire
+    -sondage
+    -créer
+    -modifier
+    -supprimer
+    -email
+    -envoyer
+    -mot de passe
+    -voir
+    -lien url
+    -date (temps défini)
+    -type
+
+
+entitée :
+question
+réponse
+
+
+
+
---
+++ documents/documents_techniques/cahier_des_charges/cahier_des_charges.groovy
@@ -0,0 +1,38 @@
+             Titre: Projet_Quideance.
+
+             Nom de l’application : Quideance
+ 
+Table des matières
+Nom de l’application :  Quideance
+L'application donnera à l"utilisateur la possibilité de répondre à un
+questionnaire ou un sondage envoyé via un lien url.
+
+Le questionnaire:
+    - Un titre
+    - Il devra être rempli dans un espace temps défini.
+    - Un résumé descriptif du questionnaire ou du sondage.
+
+
+Il y aura deux types d'aministrateurs pour gérer le site.
+L'admin pourra :
+    - se connecter 
+    - se déconnecter
+    - créer son questionnaire ou son sondage
+    - modifier son questionnaire ou son sondage 
+    - supprimer son questionnaire ou son sondage
+    - voir tous les questionnaires ou sondages de l'application
+    - modifier son mot de passe
+    - envoyer un questionnaire , sondage via un lien url
+    - faire le traitement des résultats
+
+Le super admin aura les mêmes droits que l'admin et pourra en plus :
+    - modifier tous les questionnaires ou son sondages
+    - supprimer tous questionnaires ou son sondages
+    - créer un utilisateur (profil admin ,super admin)
+    - modifier un utilisateur (profil admin, super admin)
+    - supprimer un utilisateur (profil admin, super admin)
+
+        
+
+
+
