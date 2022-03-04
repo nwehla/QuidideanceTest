0a9fb3738e8000362f6db49cff904282ac69b474
nwehla <nwehlabatoum@laposte.net>
2022-03-04 16:41:34 +0100

    cahier des charges

--- documents/documents_techniques/cahier_des_charges/cahier_des_charges.groovy
+++ documents/documents_techniques/cahier_des_charges/cahier_des_charges.groovy
@@ -5,29 +5,47 @@
 Table des matières
 Nom de l’application :  Quideance
 L'application donnera à l"utilisateur la possibilité de répondre à un
-questionnaire ou un sondage envoyé via un lien url.
+questionnaire envoyé via un lien url.
 
-Le questionnaire:
-    - Un titre
-    - Il devra être rempli dans un espace temps défini.
-    - Un résumé descriptif du questionnaire ou du sondage.
+L'espace de connexion il sera demandé :
+    - un email
+    - un mot de passe 
 
+Le mot de passe :
+    - il devra être réinialisé lors de la première connexion pour chaque admin 
+    ou super admin crée
+    - être composé d'au minimum 6 carractères, avec des chiffres  des lettres et caractères spéciaux
+    - faire une fonction mot de passe oublié
+
+Le questionnaire sera composé :
+    - un titre
+    - il devra être rempli dans un espace temps défini.
+    Cet espace temps pourra être arrêté à tout moment mais 
+    le formulaire restera visible.
+    - un résumé descriptif du questionnaire.
+    - sera composé d'une ou plusieurs questions.
+    - le formulaire devra être rempli et envoyé de façon unique
+
+Les réponses du formulaire pourront être de différent type :
+    - une zone de texte aréa pour écrire librement
+    - des boutons radio (réponse en booléen)
+    - choix multiple
 
 Il y aura deux types d'aministrateurs pour gérer le site.
 L'admin pourra :
     - se connecter 
     - se déconnecter
-    - créer son questionnaire ou son sondage
-    - modifier son questionnaire ou son sondage 
-    - supprimer son questionnaire ou son sondage
-    - voir tous les questionnaires ou sondages de l'application
+    - créer son questionnaire
+    - modifier son questionnaire
+    - supprimer son questionnaire
+    - voir tous les questionnaires de l'application
     - modifier son mot de passe
-    - envoyer un questionnaire , sondage via un lien url
+    - envoyer un questionnaire via un lien url
     - faire le traitement des résultats
 
 Le super admin aura les mêmes droits que l'admin et pourra en plus :
-    - modifier tous les questionnaires ou son sondages
-    - supprimer tous questionnaires ou son sondages
+    - modifier tous les questionnaires
+    - supprimer tous questionnaires
     - créer un utilisateur (profil admin ,super admin)
     - modifier un utilisateur (profil admin, super admin)
     - supprimer un utilisateur (profil admin, super admin)
