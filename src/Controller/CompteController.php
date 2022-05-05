df81fefc1a61cd3536068d9b9965577dedcccb53
Yugo1981 <foller.fabrice@gmail.com>
2022-05-05 17:34:27 +0200

    Probleme modifier mon compte dans dans comptecontroller à revoir grosse fatigue

--- src/Controller/CompteController.php
+++ src/Controller/CompteController.php
@@ -2,14 +2,15 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
+use App\Entity\Utilisateur;
 use App\Form\ChangePasswordType;
-use App\Repository\UserRepository;
+use App\Form\UtilisateurModifierType;
 use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\UtilisateurRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints\DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
@@ -23,8 +24,7 @@ class CompteController extends AbstractController
     public function __construct(EntityManagerInterface $entityManager) {
         $this->entityManager = $entityManager;
         // $this->user = $user;
-    }
-
+    } 
     
     /**
      * @Route("/", name="compte")
@@ -34,9 +34,9 @@ class CompteController extends AbstractController
         return $this->render('compte/compte_index.html.twig');
     }
     /**
-     * @Route("/modifier", name="compte_password")
+     * @Route("/modifier-mon-mot-de-passe", name="compte_password")
      */
-    public function modifier(Request $request, UserPasswordHasherInterface $encoder): Response
+    public function modifierpassword(Request $request, UserPasswordHasherInterface $encoder): Response
     {
         $notification = null;
         //Récupération de l'utilisateur
@@ -74,4 +74,100 @@ class CompteController extends AbstractController
             'notification' => $notification
         ]);
     }
+
+    /**
+     * @Route("/modifier-mon-compte", name="compte_edit", methods={"GET", "POST"})
+     */
+    public function modifiermesinfos(Request $request, Utilisateur $utilisateur, UtilisateurRepository $utilisateurRepository, EntityManagerInterface $manager): Response
+    {
+        // if ($this->isGranted($this->getUser())) {
+        // $form = $this->createForm(UtilisateurModifierType::class, $utilisateur);
+        // $form->handleRequest($request);
+
+        // if ($form->isSubmitted() && $form->isValid()) {
+        //     //initialisation de la date de creation 
+        //     $utilisateur->setDatemiseajour(new DateTime());
+            
+        //     //Persist
+        //     $manager->persist($utilisateur);
+             
+        //     //Flush
+        //     $manager->flush();
+        //     $utilisateurRepository->add($utilisateur);
+        //     $this->addFlash("success","La modification a été effectuée");
+        //     return $this->redirectToRoute('compte_index', [], Response::HTTP_SEE_OTHER);
+        // }
+
+        // return $this->render('compte/compte_edit.html.twig', [
+        //     'utilisateur' => $utilisateur,
+        //     'form' => $form->createView(),
+        // ]);
+        // }
+        // else{
+        //     return $this->redirectToRoute('app_accueil', [], Response::HTTP_SEE_OTHER);
+        // }
+
+        // //TEST
+        $utilisateur = $this->getUser();
+        dd($utilisateur);
+        //Appel du formulaire
+        $form = $this->createForm(UtilisateurModifierType::class , $utilisateur);
+
+        //Traitement du formulaire
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+                //initialisation de la date de creation 
+            
+            //Persist
+            $manager->persist($utilisateur);
+             
+            //Flush
+            $manager->flush();              
+            }
+                
+        return $this->render('compte/compte_edit.html.twig' , [
+            'utilisateur'=> $utilisateur,
+            'id' =>$utilisateur-> $this->getId(),
+            // 'id' => $utilisateur->getId(),
+            'form' => $form->createView(),
+        ]);    
+    }
 }
+
+
+
+
+// /**
+//      * @Route("/profil/utilisateur/{slug}/modifier", name="profil_utilisateur_modifier", methods={"GET" , "POST"})
+//      */
+
+//     public function modifier(Request $request, Utilisateur $utilisateur, EntityManagerInterface $manager, UserPasswordEncoderInterface $encoder, UtilisateurRepository $repo)
+//     {
+//         // Creation de mon Formulaire
+//         $form = $this->createForm(UtilisateurType::class, $utilisateur);
+
+//         // Analyse des Requetes & Traitement des information 
+//         $form->handleRequest($request);
+
+//         if ($form->isSubmitted() && $form->isValid()) {
+
+//             $passwordcrypter = $encoder->encodePassword($utilisateur, $utilisateur->getPassword());
+//             $utilisateur->setPassword($passwordcrypter);
+//             // $manager->persist($utilisateur); 
+//             $manager->flush();
+
+//             return $this->redirectToRoute(
+//                 'utilisateur_montre',
+//                 ['slug' => $utilisateur->getSlug()]
+//             ); // Redirection vers la page
+//         }
+//         // Redirection du Formulaire vers le TWIG pour l’affichage avec
+//         return $this->render('profil/utilisateur_modification.html.twig', [
+//             "utilisateur" => $utilisateur,
+
+//             'formUtilisateur' => $form->createView(),
+
+//             'slug' => $utilisateur->getSlug(),
+//         ]);
+//     }
